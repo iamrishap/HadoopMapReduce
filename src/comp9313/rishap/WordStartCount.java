f89e4ecb1454a9306bd352f7f04271d3ab698d0d
iamrishap <iamrishap@gmail.com>
2018-06-22 15:35:32 +1000

    Implementation in Hadoop to count the number of words starting with a particular character

--- src/comp9313/rishap/WordStartCount.java
+++ src/comp9313/rishap/WordStartCount.java
@@ -3,51 +3,58 @@ package comp9313.rishap;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
 
-public class WordCount {
-	
+public class WordStartCount {
 	public static class WordTokenizer extends Mapper<Object, Text, Text, IntWritable>{
-		private final static IntWritable one = new IntWritable(1);
+		public static IntWritable one = new IntWritable(1);
+		public static Character c;
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
 			StringTokenizer inputWords = new StringTokenizer(value.toString());
 			while(inputWords.hasMoreTokens()){
-				context.write(new Text(inputWords.nextToken()), one);
+				c = inputWords.nextToken().charAt(0);
+				c = Character.toLowerCase(c);
+				if(Character.isLetter(c)){
+					context.write(new Text(c.toString()), one);
+				}
 			}
 		}
+		
 	}
 	
-	public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+	public static class WordStartCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
 		private IntWritable result = new IntWritable();
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)  throws IOException, InterruptedException{
 			int sum = 0;
 			for(IntWritable value: values){
 				sum += value.get();
 			}
 			result.set(sum);
 			context.write(key, result);
+			
 		}
 	}
 	
 	public static void main(String[] args) throws Exception{
 		Configuration conf = new Configuration();
-	    Job job = Job.getInstance(conf, "word count");
-	    job.setJarByClass(WordCount.class);
+		Job job = Job.getInstance(conf, "word start count");
+		job.setJarByClass(WordStartCount.class);
 	    job.setMapperClass(WordTokenizer.class);
-	    job.setReducerClass(WordCountReducer.class);
+	    job.setReducerClass(WordStartCountReducer.class);
 	    job.setOutputKeyClass(Text.class);
 	    job.setOutputValueClass(IntWritable.class);
 	    FileInputFormat.addInputPath(job, new Path(args[0]));
 	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
 	    System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 	}
 
 }
